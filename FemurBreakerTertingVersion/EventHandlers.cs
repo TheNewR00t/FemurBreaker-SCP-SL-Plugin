f090a0a8fa0f479c1382c998810115c17cb4be52
TheNewR00t <126024362+TheNewR00t@users.noreply.github.com>
2025-08-22 15:08:24 +0200

    Rework

--- FemurBreakerTertingVersion/EventHandlers.cs
+++ FemurBreakerTertingVersion/EventHandlers.cs
@@ -1,15 +1,16 @@
-﻿using Exiled.API.Features;
-using System.Collections.Generic;
-using PlayerRoles;
-using Random = UnityEngine.Random;
-using Player = Exiled.API.Features.Player;
-using System.Linq;
-using Exiled.Events.EventArgs.Server;
-using MEC;
-using AudioPlayer.API;
-using ProjectMER.Events.Arguments;
+﻿using AudioPlayer.API;
 using AudioPlayer.API.Container;
 using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Server;
+using MEC;
+using PlayerRoles;
+using ProjectMER.Events.Arguments;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Player = Exiled.API.Features.Player;
+using Random = UnityEngine.Random;
 
 namespace FemurBreakerTertingVersion
 {
@@ -20,117 +21,110 @@ namespace FemurBreakerTertingVersion
         float playerAlive = 0;
         public void OnTrap(ButtonInteractedEventArgs ev)
         {
+            if (ev.Button.GameObject.name != "Button106") return;
 
-            if (ev.Button.GameObject.name == "Button106")
+            switch (playerAlive)
             {
-                if (playerAlive == 0)
-                {
-                    playerAlive = 1;
-                    if (plugin.Config.CassieWithSacrificie)
-                    {
-                        ev.Player.Kill(plugin.Config.OnSacrificeDeathReason, plugin.Config.CassieAnounceWhitPlayerDead);
-                    }
-                    else
-                    {
-                        ev.Player.Kill(plugin.Config.OnSacrificeDeathReason);
-                    }
-                    
-                    return;
-                }
-
-                if (playerAlive == 1)
-                {
-                    if (plugin.Config.UseGenerators)
-                    {
-                        if (Generator.Get(GeneratorState.Activating).Count() == plugin.Config.Generators)
-                        {
-                            if (Random.Range(1, 101) > plugin.Config.porcent)
-                            {
-                                playerAlive = 0;
-                                ev.Player.SendBroadcast(plugin.Config.OnFailure, 4);
-                            }
-                            else
-                            {
-                                playerAlive = 3;
-                                ev.Player.SendBroadcast(plugin.Config.OnDeath, 4);
-                                List<Player> scp106 = Player.List.Where(p => p.Role == RoleTypeId.Scp106).ToList();
-                                if (scp106 != null)
-                                {
-                                    foreach (Player player in scp106) {
-                                        player.Hurt(9999, DamageType.FemurBreaker);
-                                    }
-                                    Extension(plugin.Config.npc);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            ev.Player.SendBroadcast($"{plugin.Config.TextGenerators}" + $"{Generator.Get(GeneratorState.Activating).Count()} / {plugin.Config.Generators}", 4);
-                        }
-                    }
-                    else
-                    {
-                        if (Random.Range(1, 101) > plugin.Config.porcent)
-                        {
-                            playerAlive = 0;
-                            ev.Player.SendBroadcast(plugin.Config.OnFailure, 4);
-                        }
-                        else
-                        {
-                            playerAlive = 3;
-                            ev.Player.SendBroadcast(plugin.Config.OnDeath, 4);
-                            List<Player> scp106 = Player.List.Where(p => p.Role == RoleTypeId.Scp106).ToList();
-                            if (scp106 != null)
-                            {
-                                foreach (Player player in scp106) { player.Kill(plugin.Config.OnRecontainmentDeath); }
-                                Extension(plugin.Config.npc);
-                            }
-                        }
-                    }
-
-
-                }
-                else
-                {
-                    if (playerAlive == 3)
-                    {
-                        ev.Player.SendBroadcast(plugin.Config.OnRecontainmentRepeat, 4);
-                    }
-                    else
-                    {
-                        ev.Player.SendBroadcast(plugin.Config.OnRequirements, 4);
-                    }
-                }
+                case 0:
+                    HandleFirstInteraction(ev.Player);
+                    break;
+                case 1:
+                    HandleSecondInteraction(ev.Player);
+                    break;
+                case 3:
+                    ev.Player.SendBroadcast(plugin.Config.OnRecontainmentRepeat, 4);
+                    break;
+                default:
+                    ev.Player.SendBroadcast(plugin.Config.OnRequirements, 4);
+                    break;
             }
         }
-        
-        public void Extension(bool YT)
+
+        private void HandleFirstInteraction(Player player)
         {
-            if (plugin.Config.SoundOrNotsound) 
+            playerAlive = 1;
+            if (plugin.Config.CassieWithSacrificie)
             {
-                if (YT)
-                {
-                    var path = System.IO.Path.Combine(Paths.Plugins, "Audio", plugin.Config.FileAudioName + ".ogg");
-                    var npc = AudioPlayerBot.SpawnDummy(name: plugin.Config.OnNameBot, id: 99, badgeText: plugin.Config.BotBadgetName, bagdeColor: plugin.Config.BadgetBotColor);
-                    npc.Player.ReferenceHub.roleManager.ServerSetRole(RoleTypeId.Overwatch, RoleChangeReason.Died);
-                    var audio = npc.AudioPlayerBase;
-                    audio.BroadcastChannel = VoiceChat.VoiceChatChannel.Intercom;
-                    audio.AudioToPlay.Add(path);
-                    audio.Play(0);
-                    Timing.CallDelayed(plugin.Config.seconds, () =>
-                    {
-                        AudioController.DisconnectDummy(npc.ID);
-                    });
-                }
-                else
-                {
-                    return;
-                }
+                player.Kill(plugin.Config.OnSacrificeDeathReason, plugin.Config.CassieAnounceWhitPlayerDead);
             }
             else
             {
-                Cassie.Message(plugin.Config.Cassie);
+                player.Kill(plugin.Config.OnSacrificeDeathReason);
             }
+        }
+
+        private void HandleSecondInteraction(Player player)
+        {
+            bool success = Random.Range(1, 101) <= plugin.Config.porcent;
+
+            if (plugin.Config.UseGenerators)
+            {
+                int activeGenerators = Generator.Get(GeneratorState.Activating).Count();
+                if (activeGenerators != plugin.Config.Generators)
+                {
+                    player.Broadcast(4, $"{plugin.Config.TextGenerators}{activeGenerators} / {plugin.Config.Generators}");
+                    return;
+                }
+            }
+
+            if (!success)
+            {
+                playerAlive = 0;
+                player.Broadcast(4, plugin.Config.OnFailure);
+            }
+            else
+            {
+                playerAlive = 3;
+                player.Broadcast(4, plugin.Config.OnDeath);
+                AffectScp106();
+                Extension(plugin.Config.npc);
+            }
+        }
+
+        private void AffectScp106()
+        {
+            List<Player> scp106 = Player.List.Where(p => p.Role == RoleTypeId.Scp106).ToList();
+            foreach (Player p in scp106)
+            {
+                if (plugin.Config.UseGenerators)
+                    p.Hurt(9999, DamageType.FemurBreaker);
+                else
+                    p.Kill(plugin.Config.OnRecontainmentDeath);
+            }
+        }
+
+        public void Extension(bool YT)
+        {
+            if (!plugin.Config.SoundOrNotsound)
+            {
+                Cassie.Message(plugin.Config.Cassie);
+                return;
+            }
+
+            if (!YT) return;
+
+            PlayAudioBot(plugin.Config.FileAudioName, plugin.Config.OnNameBot, plugin.Config.BotBadgetName, plugin.Config.BadgetBotColor, plugin.Config.seconds);
+        }
+
+        private void PlayAudioBot(string fileName, string botName, string badgeText, string badgeColor, float duration)
+        {
+            string path = Path.Combine(Paths.Plugins, "Audio", fileName + ".ogg");
+
+            var npc = AudioPlayerBot.SpawnDummy(
+                name: botName,
+                id: 99,
+                badgeText: badgeText,
+                bagdeColor: badgeColor
+            );
+
+            npc.Player.ReferenceHub.roleManager.ServerSetRole(RoleTypeId.Overwatch, RoleChangeReason.Died);
+
+            var audio = npc.AudioPlayerBase;
+            audio.BroadcastChannel = VoiceChat.VoiceChatChannel.Intercom;
+            audio.AudioToPlay.Add(path);
+            audio.Play(0);
+
+            Timing.CallDelayed(duration, () => AudioController.DisconnectDummy(npc.ID));
         }
 
         public void OnRestart(RoundEndedEventArgs ev)
